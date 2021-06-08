79ad477fbc051aef8f414b339c890f772ce8de88
Ruth Mayodi <ruthmayodi@gmail.com>
2021-06-08 18:00:55 -0400

    refactoring code still broke it

--- src/api/index.js
+++ src/api/index.js
@@ -10,11 +10,9 @@ export const fetchData = async (country) => {
   }
 
   try {
-    const {
-      data: { confirmed, recovered, deaths, lastUpdate },
-    } = await axios.get(changeableUrl);
+    const data = await axios.get(changeableUrl);
 
-    return { confirmed, recovered, deaths, lastUpdate };
+    return { data };
   } catch (error) {
     return error;
   }
